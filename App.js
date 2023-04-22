042d9f23e2b7970e2ae29e989d981829dc663f89
Rishi Meka <rishimeka@Rishis-MacBook-Air.local>
2023-04-21 22:18:15 -0500

    Completed Onboarding pages and navigation pages

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
-import { Image } from 'expo-image';
-import * as Font from 'expo-font';
 import React from "react";
-import { Ionicons } from '@expo/vector-icons';
 import Onboarding_Page_1 from "./Pages/Onboarding_Page_1";
+import { NavigationContainer } from '@react-navigation/native';
+import Navigator from "./Navigator";
 
 export default function App() {
-    return <Onboarding_Page_1/>
+    return <Navigator/>
 }
