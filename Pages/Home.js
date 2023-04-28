0fa4f95cae6c6d96311393071ca0d97d7bf0277c
Rahik-Sikder <96138506+Rahik-Sikder@users.noreply.github.com>
2023-04-27 19:56:50 -0500

    Created HomePage

---
+++ Pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View>
+        <Text>
+            Hello, [User] welcome to Healm
+        </Text>
+    </View>
+  )
+}
+
+export default Home
